5f6f429d5966295291dc869bb7518fd0f3bf5021
ArchageXIII <mark.3dwards@gmail.com>
2016-12-29 21:38:43 +0000

    updated setup.py with download location

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ config = {
     'description': ('Store and retreive any file data in any image'),
     'author': 'Mark Edwards',
     'url': 'https://github.com/ArchageXIII/pillowncase',
-    'download_url': 'https://github.com/ArchageXIII/pillowncase',
+    'download_url': 'https://github.com/ArchageXIII/pillowncase/blob/master/dist/pillowncase-0.1.tar.gz',
     'author_email': 'mark.3dwards@gmail.com',
     'version': '0.1',
     'licence': 'GNU GPL',
